ce465fd89907091ba81b7d4acbdf101f58fae746
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-12 12:46:51 -0300

    Definiendo prioridad para el template personalizado.

--- v2ex_settings.py
+++ v2ex_settings.py
@@ -10,9 +10,9 @@ import lbforum
 V2EX_TEMPLATE_DIR = os.path.join(lbforum.__path__[0], 'templates_v2ex')
 
 TEMPLATE_DIRS = (
-        os.path.join(HERE, 'templates_plus'),
+        #os.path.join(HERE, 'templates_plus'),
+        #V2EX_TEMPLATE_DIR,
         os.path.join(HERE, 'templates_v2ex'),
-        V2EX_TEMPLATE_DIR,
 )
 
 try:
