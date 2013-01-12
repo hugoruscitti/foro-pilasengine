115ad185f35229fa3e55397895cd1f5492504274
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-12 12:48:38 -0300

    cambiando ruta a templates.

--- settings.py
+++ settings.py
@@ -121,8 +121,8 @@ import lbforum
 V2EX_TEMPLATE_DIR = os.path.join(lbforum.__path__[0], 'templates_v2ex')
 
 TEMPLATE_DIRS = (
-        os.path.join(HERE, 'templates_plus'),
-        os.path.join(HERE, 'templates'),
+        #os.path.join(HERE, 'templates_plus'),
+        #os.path.join(HERE, 'templates'),
         V2EX_TEMPLATE_DIR,
 )
 
